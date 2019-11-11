007749eb85efd0279cf84aca667b72c697fbfec8
superAlan <depeng.luan.cmu@gmail.com>
2019-11-11 14:21:38 -0800

    conquered lc 729 my calendar

---
+++ src/main/java/topics/scan_line/LC_729_My_Calendar.java
@@ -0,0 +1,24 @@
+package topics.scan_line;
+
+import java.util.TreeMap;
+
+public class LC_729_My_Calendar {
+
+  private TreeMap<Integer, Integer> treeMap;
+
+  public LC_729_My_Calendar() {
+    treeMap = new TreeMap<>();
+  }
+
+  public boolean book(int start, int end) {
+    Integer preStart = treeMap.floorKey(start);
+    Integer nextStart = treeMap.ceilingKey(start);
+    if ((start > end)
+        || (preStart != null && treeMap.get(preStart) > start)
+        || (nextStart != null && nextStart < end)) {
+      return false;
+    }
+    treeMap.put(start, end);
+    return true;
+  }
+}
