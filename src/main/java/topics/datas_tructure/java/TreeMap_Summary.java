5655e4ffda022a346e815230bb85827d147af715
superAlan <depeng.luan.cmu@gmail.com>
2019-11-12 21:59:09 -0800

    added some examples of TreeMap

--- src/main/java/topics/datas_tructure/java/TreeMap_Summary.java
+++ src/main/java/topics/datas_tructure/java/TreeMap_Summary.java
@@ -1,5 +1,35 @@
 package topics.datas_tructure.java;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import topics.scan_line.model.Interval;
+
+// ordered, self-balanced tree
 public class TreeMap_Summary {
-  // TODO: check out the use case of TreeMap and relevant Leetcode problems.
+
+  public void commonUse() {
+    TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+    insertIntervals(treeMap);
+
+    int target = 7;
+    Integer preStart = treeMap.floorKey(target);     // could be null
+    Integer nextStart = treeMap.ceilingKey(target);  // could be null
+
+    Map.Entry<Integer, Integer> preEntry = treeMap.floorEntry(target);
+    Map.Entry<Integer, Integer> nextEntry = treeMap.ceilingEntry(target);
+  }
+
+  private void insertIntervals(TreeMap<Integer, Integer> treeMap) {
+    List<Interval> intervals = new LinkedList<>(Arrays.asList(
+        new Interval(2, 5),
+        new Interval(3, 8),
+        new Interval(9, 13)
+    ));
+    intervals.forEach((interval) -> {
+      treeMap.put(interval.start, interval.end);
+    });
+  }
 }
