56591fee790727fc64a6ab2147a9ef5e25968c0d
superAlan <depeng.luan.cmu@gmail.com>
2019-11-26 18:45:28 -0800

    HashMap thread safe issue

--- src/main/java/topics/datas_tructure/java/HashMap_Summary.java
+++ src/main/java/topics/datas_tructure/java/HashMap_Summary.java
@@ -1,8 +1,10 @@
 package topics.datas_tructure.java;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 public class HashMap_Summary {
 
@@ -24,6 +26,34 @@ public class HashMap_Summary {
     for(Map.Entry<String, String> entry : map.entrySet()) {
       String key = entry.getKey();
       String value = entry.getValue();
+    }
+  }
+
+  public void concurrent() {
+
+    Map<String, String> nonThreadSafeMap = new HashMap<>();
+    /*
+     * If multiple threads access a (linked) hash map concurrently,
+     * and at least one of the threads modifies the map structurally,
+     * it must be synchronized externally.
+     *
+     * This is typically accomplished by synchronizing on some object that naturally encapsulates the map.
+     * If no such object exists, the map should be "wrapped" using the Collections.synchronizedMap method.
+     * This is best done at creation time, to prevent accidental unsynchronized access to the map
+     */
+
+    Map<String, String> threadSafeMap = Collections.synchronizedMap(new HashMap<String, String>());
+
+    // However it is not enough to make it fully thread-safe
+    // you sill need to protect any iteration over the content of the map using the instance of the wrapped map as object's monitor
+
+    Map m = Collections.synchronizedMap(threadSafeMap);
+    Set s = m.keySet();  // Needn't be in synchronized block
+    synchronized (m) {   // Synchronizing on m, not s!
+      Iterator i = s.iterator(); // Must be in synchronized block
+      while (i.hasNext()) {
+        System.out.println(i.next());
+      }
     }
   }
 
